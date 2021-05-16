31560c077325b1013e2d0af8a156a1ff1828a800
MuhammadJamshaidGhaffar <muhammadjamshaid89@gmail.com>
2021-05-16 13:47:22 +0500

    Added Forecast

--- js/script.js
+++ js/script.js
@@ -8,9 +8,11 @@ let humidity = document.querySelector('.weather-indicator-humidity>.value');
 let wind = document.querySelector('.weather-indicator-wind>.value');
 let pressure = document.querySelector('.weather-indicator-pressure>.value');
 let image = document.querySelector('.weather-image');
-let temperature = document.querySelector('.weather-temp>.value');
+let temperature = document.querySelector('.weather-temp .value');
 
 
+let forecastBaseEndpoint = "https://api.openweathermap.org/data/2.5/forecast?units=metric&appid=" + weatherApi;
+let forecastBlock = document.querySelector('.weather-forecast');
 
 let getWeatherByCityName =  async (cityName) =>
 {
@@ -26,14 +28,45 @@ let getWeatherByCityName =  async (cityName) =>
     }
 }
 
-// getWeatherByCityName("New York");
+let getForecastByCityName = async (cityName) =>
+{
+    try{
+
+        let endpoint = forecastBaseEndpoint +"&q=" + cityName;
+        let data = await fetch(endpoint);
+        let weather = await data.json();
+        return weather;
+    }
+    catch(err){
+        console.log("In Function getForecastByCityName()  Error is : " + err);
+    }
+}
+
 
 searchInp.addEventListener("keydown" ,async (e) =>{
     if(e.key == "Enter"){
-        console.log("Proceesing");
+        console.log("Proceesing Weather Request");
         let weather = await getWeatherByCityName(searchInp.value);
         console.log(weather);
         updateCurrentWeather(weather);
+
+        console.log("Processing Forecast Request");
+        let forecast = await getForecastByCityName(searchInp.value);
+        console.log(forecast);
+
+        let forecastList = forecast.list;
+        let daily  = [];
+        console.log("Getting 12:00 forecast list")
+        forecastList.forEach(day => {
+            let date = new Date(day.dt_txt.replace(" " , "T"));
+            if(date.getHours() == 12)
+            {
+                console.log(date);
+                daily.push(day);
+            }
+        });
+        console.log(daily);
+        updateForecast(daily);
     }
 })
 
@@ -42,6 +75,9 @@ function updateCurrentWeather (data){
     day.textContent = getDayOfWeek();
     humidity.textContent = data.main.humidity;
     pressure.textContent = data.main.pressure;
+    wind.textContent = getWindDirection(data.wind.deg) + " , " + data.wind.speed;
+    temperature.textContent = data.main.temp > 0 ? "+" + Math.round(data.main.temp) : Math.round(data.main.temp) ;
+
 }
 
 function getDayOfWeek()
@@ -50,4 +86,28 @@ function getDayOfWeek()
     return date.toLocaleDateString('en-EN' , {'weekday' : 'long'});
 }
 
-funtion get
+function getWindDirection (deg){
+    if(deg > 330 && deg <= 30)
+        return "North";
+    else if(deg > 30 && deg <= 60)
+        return "North-East";
+    else if(deg > 60 && deg <=120)
+        return "East";
+    else if(deg > 120 && deg <= 150)
+        return "South-East";
+    else if(deg > 150 && deg <= 210)
+        return "South";
+    else if(deg > 210 && deg <= 240)
+        return "South-West";
+    else if(deg > 240 && deg <= 300)
+        return "West";
+    else if(deg > 300 && deg <= 330)
+        return "North-West";
+}
+
+function updateForecast(forecast){
+    forecastBlock.innerHTML = '';
+    forecast.forEach(day => {
+        let iconUrl = "http://openweathermap.org/img/wn/"+ day.weather[0].icon+"@2x.png";
+    });
+}
